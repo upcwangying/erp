8027fdceee9851165015259c89fe91126a381b3a
ywang1988 <864891814@qq.com>
2016-09-06 18:08:31 +0800

    FileDownloadServlet.java

---
+++ src/main/java/com/erp/product/servlet/FileDownloadServlet.java
@@ -0,0 +1,31 @@
+package com.erp.product.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by wang_ on 2016-09-06.
+ */
+@WebServlet(name = "FileDownloadServlet",
+        urlPatterns = {"/FileDownloadServlet"})
+public class FileDownloadServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=UTF-8");
+
+
+    }
+}
